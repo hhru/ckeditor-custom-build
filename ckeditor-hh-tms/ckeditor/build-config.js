b31473a3eaada44082e17311570b9fca48cc3eeb
sergdenisov <xsergeantx@mail.ru>
2019-10-11 17:08:33 +0300

    HH-90149 Up tms CKEditor version to 4.13.0

--- ckeditor-hh-tms/ckeditor/build-config.js
+++ ckeditor-hh-tms/ckeditor/build-config.js
@@ -1,6 +1,6 @@
 ﻿/**
- * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/license
  */
 
 /**
@@ -10,13 +10,13 @@
  * If you would like to build CKEditor online again
  * (for example to upgrade), visit one the following links:
  *
- * (1) http://ckeditor.com/builder
+ * (1) https://ckeditor.com/cke4/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/8099ed551905966b678c18c4e9f08da0
+ * (2) https://ckeditor.com/cke4/builder/559a6c95a2ca1b2c7dce7348dfd676fc
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/8099ed551905966b678c18c4e9f08da0
+ * (3) https://ckeditor.com/cke4/builder/download/559a6c95a2ca1b2c7dce7348dfd676fc
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -38,6 +38,7 @@ var CKBUILDER_CONFIG = {
 		'.jshintignore',
 		'.jshintrc',
 		'.mailmap',
+		'.npm',
 		'.travis.yml',
 		'bender-err.log',
 		'bender-out.log',
